8be9a161f32859dc034cf5c03952959fb5b34517
Maryam <cheikhamariamba@gmail.com>
2026-04-03 16:15:02 +0000

    Enregistrement des modeles dans admin et personnalisation

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Categorie, Article
 
 # Register your models here.
+
+@admin.register(Categorie)
+class CategorieAdmin(admin.ModelAdmin):
+    list_display = ('id','nom',)
+    search_fields = ('nom',)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('titre','contenu','date_publication')
+    list_filter = ('categories', 'date_publication')
+    search_fields = ('titre',)
+    date_hierarchy = 'date_publication'
+
+
+admin.site.register(Article,ArticleAdmin)
+
+
