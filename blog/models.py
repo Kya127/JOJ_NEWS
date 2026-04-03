9bafc70f63b6c5e52b98c07753a75c2aea917803
Maryam <cheikhamariamba@gmail.com>
2026-04-03 15:37:57 +0000

    Ajout des modeles Categorie et Article

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Categorie(models.Model):
+    nom = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nom
+    
+class Article(models.Model):
+    titre = models.CharField(max_length=200)
+    contenu = models.TextField()
+    date_publication = models.DateTimeField(auto_now_add=True)
+    auteur = models.ForeignKey(User,on_delete=models.CASCADE)
+    categories = models.ForeignKey(Categorie,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titre
+    
