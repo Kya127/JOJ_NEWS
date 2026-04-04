bda6d1593f230dc6629dd135f1bac0719a3af974
habii45 <habyl5430@gmail.com>
2026-04-04 22:38:23 +0000

    ajout de la vue listview et du template liste.html

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Accueil, name = 'accueil'),
     path('incrire/', views.Inscription.as_view(), name='inscrire'),
+    path('liste/',views.ListView,name='liste_des_articles'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .models import Article
 
 # Create your views here.
 def Accueil(request):
@@ -14,3 +15,9 @@ class Inscription(CreateView):
     success_url = reverse_lazy('accueil')
     template_name = 'registration/signup.html'
 
+def ListView(request):
+    liste_article=Article.objects.all()
+    context={
+        'liste_article':liste_article
+    }
+    return render(request,'liste.html',context)
