16f5c0cf14a3dbf7d809334290fdeb274d674339
habii45 <habyl5430@gmail.com>
2026-04-05 00:18:05 +0000

    creation de la vue et du template pour afficher les details sur un article

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
@@ -21,3 +21,10 @@ def ListView(request):
         'liste_article':liste_article
     }
     return render(request,'liste.html',context)
+
+def DetailView(request,id):
+    un_article=get_object_or_404(Article,id=id)
+    article_recuperee={
+        'un_article':un_article
+    }
+    return render(request,'detail.html',article_recuperee)
